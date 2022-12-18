20eb6ffa2db57c8e2293e16a2b0ab64d8c627f14
Dave <davidlongswe@gmail.com>
2022-12-19 00:14:06 +0100

    stripe configured

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import React from "react";
-
+import { StateContext } from "../context/StateContext";
 import { Layout } from "../components";
 import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   );
 }
